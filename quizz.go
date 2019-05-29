42b243da7f814fd95cb88f1554e7263d352e313f
riphidon <riphidon.inc@gmail.com>
2019-05-29 12:01:49 +0200

    part 2, timer implementatiion

--- quizz.go
+++ quizz.go
@@ -7,36 +7,57 @@ import (
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	var s bool
+	var stop bool
 	var res string
 	var count int
 	var total = getNbQuestions()
 	var file = readFile()
-	fmt.Println(total)
-	for {
-		record, err := file.Read()
-		if err == io.EOF {
-			if count > 10 {
-				fmt.Printf("You scored %v out of %v, good job\n", count, total)
-			} else {
-				fmt.Printf("You scored %v out of %v, try again\n", count, total)
+
+	fmt.Println("Press start")
+	gameStart := bufio.NewScanner(os.Stdin)
+	if gameStart.Scan() {
+		s = true
+	}
+	if s != false {
+		timer := time.AfterFunc(10*time.Second, func() {
+			stop = true
+			fmt.Println("\nTime is over")
+		})
+		defer timer.Stop()
+		for {
+
+			record, err := file.Read()
+			if stop == true || err == io.EOF {
+				if count > 10 {
+					fmt.Printf("You scored %v out of %v, good job\n", count, total)
+				} else {
+					fmt.Printf("You scored %v out of %v, try again\n", count, total)
+				}
+				break
 			}
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("What %v is equal to?\n", record[0])
-		scanner := bufio.NewScanner(os.Stdin)
-		if scanner.Scan() {
-			res = scanner.Text()
-		}
-		if res != record[1] {
-			fmt.Printf("Wrong Answer!! %v equals %s\n", record[0], record[1])
-		} else {
-			count++
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if stop == false {
+				fmt.Printf("What %v is equal to?\n", record[0])
+				scanner := bufio.NewScanner(os.Stdin)
+				if scanner.Scan() {
+					res = scanner.Text()
+					if res != record[1] {
+						fmt.Printf("Wrong Answer!! %v equals %s\n", record[0], record[1])
+					} else {
+						fmt.Printf("Correct %v equals %s\n", record[0], record[1])
+						count++
+					}
+				}
+			}
+
 		}
 
 	}
